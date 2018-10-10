48366cf598fac02ecb327e13cb107b061fb961f0
H4KRO <eddie.vallier@gmail.com>
2018-10-10 18:00:50 +0200

    First working version

--- modal.js
+++ modal.js
@@ -1,10 +1,45 @@
-function openModal(imgsrc){
+function openModal(imageToLoad){
 	var modal = document.getElementById("modal");
 	modal.style.display = "block";
 	modal.style.animationName = "modalIn";
 	modal.style.animationDuration = "1s";
-	document.getElementById("modalImageToDisplay").src = imgsrc;
-	alert(i);
+
+	var imageToDisplay = document.getElementById("modalImageToDisplay");
+	imageToDisplay.src = imageToLoad.src;
+	imageToDisplay.setAttribute("imageId", imageToLoad.getAttribute("imageId"));
+}
+
+function nextImage(){
+	var currentId = document.getElementById("modalImageToDisplay").getAttribute("imageId");
+	var nbOfImages = document.getElementsByClassName("modalImage").length;
+	var nextId = 0;
+	if(currentId + 1 < nbOfImages){
+		nextId++;
+	}
+	openImage(nextId);
+}
+
+function previousImage(){
+	var currentId = document.getElementById("modalImageToDisplay").getAttribute("imageId");
+	var nbOfImages = document.getElementsByClassName("modalImage").length;
+	var previousId = nbOfImages-1;
+	if(currentId - 1 >= 0){
+		previousId = previousId - 1;
+	}
+	openImage(previousId);
+}
+
+function openImage(newId){
+	var imageToDisplay = document.getElementById("modalImageToDisplay");
+
+	var images = document.getElementsByClassName("modalImage");
+	for(var i = 0; i < images.length; i++){
+		if(i == newId){
+			imageToDisplay.src = images[i].src;
+			imageToDisplay.setAttribute("imageId", newId);
+		}
+	}
+	
 }
 
 function closeModal(){
@@ -21,7 +56,7 @@ function modalInit(){
 	for(var i = 0; i < images.length; i++){
 		images[i].setAttribute("imageId", i);
 		images[i].onclick = function(){
-			openModal(this.src);
+			openModal(this);
 		}
 	}
 }
