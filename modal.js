c317b45bea284c981d1465768d07b336adc31727
valliere <eddie.vallier@etu.umontpellier.fr>
2018-10-17 13:28:46 +0200

    First optimization !

--- modal.js
+++ modal.js
@@ -1,9 +1,17 @@
+var modal;
+var modalOpen;
+var images;
+var currentImageId;
+var modalImage;
+var imageName;
+var imageDesc;
+var slideshowSpeed;
 var speed = 1000;
 var isPlaying = false;
 var modalOpen = false;
 
 function updateSpeed(){
-	document.getElementById("slideshowSpeed").innerText = speed/1000 + "s";
+	slideshowSpeed.innerText = speed/1000 + "s";
 }
 
 function increaseSpeed(){
@@ -40,89 +48,50 @@ function stopPlaying(){
 	isPlaying = false;
 }
 
+function nextImage(){
+	if(currentId == images.length - 1) openImage(0);
+	else openImage(currentId + 1);
+}
+
+function previousImage(){
+	if(currentId == 0) openImage(images.length - 1);
+	else openImage(currentId - 1); 
+}
+
+function loadImage(image){
+	modalImage.src = image.src;
+	imageName.innerText = image.getAttribute("name");
+	imageDesc.innerText = image.getAttribute("desc");
+}
+
+function openImage(imageId){
+	currentId = imageId;
+	loadImage(images[imageId]);
+}
+
 function openModal(imageToLoad){
-	var modal = document.getElementById("modal");
 	modal.style.display = "block";
 	modal.style.animationName = "modalIn";
 	modal.style.animationDuration = "1s";
 
 	document.body.style.overflow = "hidden";
 
-	openImage(parseInt(imageToLoad.getAttribute("imageId")));
+	currentImageId = parseInt(imageToLoad.getAttribute("imageId"));
+	openImage(currentImageId);
 
 	modalOpen = true;
-	slideshow();
-}
-
-function getCurrentId(){
-	return parseInt(document.getElementById("modalImageToDisplay").getAttribute("imageId"));
-}
-
-function getNbOfImages(){
-	return parseInt(document.getElementsByClassName("modalImage").length);
-}
-
-function nextImage(){
-	if(getCurrentId() == getNbOfImages()-1){
-		openImage(0);
-	}else{
-		openImage(getCurrentId()+1);	
-	}
-
-}
-
-function previousImage(){
-	if(getCurrentId() == 0){
-		openImage(getNbOfImages() - 1);
-	}else{
-		openImage(getCurrentId() - 1)
-	}
-}
-
-function openImage(newId){
-	var imageToDisplay = document.getElementById("modalImageToDisplay");
-
-	var images = document.getElementsByClassName("modalImage");
-	for(var i = 0; i < images.length; i++){
-		if(i == newId){
-			imageToDisplay.src = images[i].src;
-			imageToDisplay.setAttribute("imageId", newId);
-			document.getElementById("modalImageName").innerText = images[i].getAttribute("name");
-			document.getElementById("modalImageDesc").innerText = images[i].getAttribute("desc");
-		}
-	}
-
-
-
-    pagination.innerText = (newId+1) + "/" + getNbOfImages();
 }
 
 function closeModal(){
-	var modal = document.getElementById("modal");
 	modal.style.animationName = "modalOut";
 	modal.style.animationDuration = ".5s";
 	setTimeout(function(){
 		modal.style.display = "none";
         document.body.style.overflow = "auto";
 	}, 500);
-
-
-}
-
-function modalInit(){
-	updateSpeed();
-	var images = document.getElementsByClassName("modalImage");
-	for(var i = 0; i < images.length; i++){
-		images[i].setAttribute("imageId", i);
-		images[i].onclick = function(){
-			openModal(this);
-		}
-	}
-
 }
 
 document.onkeydown = function(e) {
-	//alert(e.keyCode);
     switch (e.keyCode) {
     	case 27:
     		closeModal();
@@ -149,6 +118,25 @@ document.onkeydown = function(e) {
     }
 };
 
+function modalInit(){
+	modal = document.getElementById("modal");
+	images = document.getElementsByClassName("modalImage");
+	slideshowSpeed = document.getElementById("slideshowSpeed");
+
+	for(var i = 0; i < images.length; i++){
+		images[i].setAttribute("imageId", i);
+		images[i].onclick = function(){openModal(this);}
+	}
+
+	modalImage = document.getElementById("modalImageToDisplay");
+	imageName = document.getElementById("modalImageName");
+	imageDesc = document.getElementById("modalImageDesc");
+
+}
+
+
+
 window.onload = function(){
 	modalInit();
 }
+
